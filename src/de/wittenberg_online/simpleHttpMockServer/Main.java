f999e4dab5abd6fedc0364847a7a693e8415ade0
Wittenberg <sven.wittenberg@gmail.com>
2018-03-25 21:35:48 +0200

    Init

---
+++ src/de/wittenberg_online/simpleHttpMockServer/Main.java
@@ -0,0 +1,17 @@
+package de.wittenberg_online.simpleHttpMockServer;
+
+import de.wittenberg_online.simpleHttpMockServer.startWindow.StartWindow;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        StartWindow sw = new StartWindow();
+
+        System.out.println("Hello World!");
+    }
+
+
+
+}
---
+++ src/de/wittenberg_online/simpleHttpMockServer/startWindow/StartWindow.java
@@ -0,0 +1,8 @@
+package de.wittenberg_online.simpleHttpMockServer.startWindow;
+
+import javax.swing.*;
+
+public class StartWindow {
+
+    private JButton button1;
+}
